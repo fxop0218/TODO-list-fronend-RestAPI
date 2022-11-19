d6536f9605a9f952fb6bfd39bc027f7b45be1600
fxop0218 <francescoli0218@gmail.com>
2022-11-19 09:52:29 +0100

    Front end added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,36 +1,61 @@
 import loginCall from "../api/call"
+import { useForm } from "react-hook-form"
+import React, { useState } from "react"
+import TextField from "@material-ui/core/TextField"
+import swal from "sweetalert"
+import { SwipeableDrawer } from "@material-ui/core"
+
+async function loginUser(credentials) {
+    return fetch("http://localhost.5000/user/login", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+        },
+        async: true,
+        body: JSON.stringify(credentials),
+    }).then((data) => data.json())
+}
 
 function Login() {
+    const [username, setUserName] = useState()
+    const [password, setPassword] = useState()
     var user_loged = sessionStorage.getItem("user_loged")
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        var loged = await loginCall(
-            event.target.elements.username.value,
-            event.target.elements.password.value
-        )
-        if (loged.ok) this.setState({ isProcessing: false })
-        console.log(`Hello: ${loged}`)
-        alert(`Submitted form ${loged}`)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const response = await loginUser({ username, password })
+        if ("accessToken" in response) {
+            swal("Success", response.message, "success", { buttons: false, timer: 2000 }).then(
+                (value) => {
+                    localStorage.setItem("accessToken", response["accessToken"])
+                    localStorage.setItem("user", JSON.stringify(response["user"]))
+                }
+            )
+        } else {
+            swal("Failed", response.message, "Error")
+        }
     }
+
     return (
         <div className="flex h-screen justify-center items-center">
             {!user_loged ? (
-                <form className="columns-1" onSubmit={handleSubmit}>
+                <form className="columns-1" noValidate onSubmit={handleSubmit}>
                     <div className="p-3">
-                        <input
+                        <TextField
                             type="text"
                             name="username"
                             placeholder="Username"
-                            className="border-black text-center h-10"
+                            onChange={(e) => setUserName(e.target.value)}
                         />
                     </div>
 
                     <div className="p-3">
-                        <input
+                        <TextField
                             type="password"
                             name="password"
                             placeholder="Password"
-                            className="border-black text-center h-10"
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                     <div className="p-3 ">
